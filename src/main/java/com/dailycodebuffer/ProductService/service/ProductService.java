cd7cc6049cc6f20de4cef740c35dbcc2ef1e26cf
Abhishek Chauthe <abhishek.chauthe@neosofttech.com>
2026-05-26 16:42:24 +0530

    created product service

---
+++ src/main/java/com/dailycodebuffer/ProductService/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.dailycodebuffer.ProductService.service;
+
+import com.dailycodebuffer.ProductService.model.ProductRequest;
+import com.dailycodebuffer.ProductService.model.ProductResponse;
+
+public interface ProductService {
+      ProductResponse getProductId(long productId);
+
+
+    long addProduct(ProductRequest productRequest);
+}
---
+++ src/main/java/com/dailycodebuffer/ProductService/service/ProductServiceImpl.java
@@ -0,0 +1,48 @@
+package com.dailycodebuffer.ProductService.service;
+
+import com.dailycodebuffer.ProductService.entity.Product;
+import com.dailycodebuffer.ProductService.model.ProductRequest;
+import com.dailycodebuffer.ProductService.model.ProductResponse;
+import com.dailycodebuffer.ProductService.repository.ProductRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.beans.BeanUtils.*;
+
+@Service
+@Log4j2
+public class ProductServiceImpl implements ProductService{
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    @Override
+    public ProductResponse getProductId(long productId) {
+        log.info("Get the product for productid: {}",productId);
+        Product product = productRepository.findById(productId).orElseThrow(() -> new RuntimeException("Product with given id not found"));
+
+        ProductResponse productResponse = new ProductResponse();
+        copyProperties(product ,productResponse);
+
+        return productResponse;
+
+    }
+
+    @Override
+    public long addProduct(ProductRequest productRequest) {
+        log.info("Adding Product.. ");
+
+        Product product = Product.builder()
+                .productName(productRequest.getName())
+                .quantity(productRequest.getQuantity())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+
+        log.info("Product Created");
+        return product.getProductId();
+    }
+}
